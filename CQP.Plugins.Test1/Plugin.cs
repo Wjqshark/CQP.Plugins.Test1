5617ae2e407ae803ab23bde3b3932c1f230e2667
wjqshark <719539302@qq.com>
2017-05-17 22:13:02 +0800

    5.17更新为第二版

--- CQP.Plugins.Test1/Plugin.cs
+++ CQP.Plugins.Test1/Plugin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Newbe.CQP.Framework;
 using Newbe.CQP.Framework.Extensions;
@@ -10,13 +11,28 @@ namespace CQP.Plugins.Test1
 {
     public class Plugin : PluginBase
     {
+        private ICoolQApi m_API;
+
+        /// <summary>
+        /// 随机数生成
+        /// </summary>
+        private Random m_Rand = new Random();
+        /// <summary>
+        /// qq群里qq号字典
+        /// </summary>
+        private Dictionary<long, List<long>> m_QQdics = new Dictionary<long, List<long>>();
+
         public override string AppId => "CQP.Plugins.Test1";
 
         public Plugin(ICoolQApi coolQApi) : base(coolQApi)
         {
+            m_API = coolQApi;
         }
+
+        #region 处理各种消息 入口
+
         /// <summary>
-        /// 处理群里信息
+        /// 处理群里信息 
         /// </summary>
         /// <param name="subType"></param>
         /// <param name="sendTime"></param>
@@ -28,97 +44,212 @@ namespace CQP.Plugins.Test1
         /// <returns></returns>
         public override int ProcessGroupMessage(int subType, int sendTime, long fromGroup, long fromQq, string fromAnonymous, string msg, int font)
         {
-            long targetqq = 0;
-
-            if (CheckHasAtQQ(msg, out targetqq))
-            {
-                if (msg.Contains("禁言"))
-                {
-                    Random rand = new Random();
-
-
-                    bool success = rand.Next(4200) % 3 == 1;
-                    if (success)
-                    {
-                        CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + "对" + CoolQCode.At(targetqq) + "释放了禁言术！ 成功了！  "+ CoolQCode.At(targetqq) +"被禁言3分钟！");
-                        CoolQApi.SetGroupBan(fromGroup, targetqq, 180);
-                    }
-                    else
-                    {
-                        CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + "对" + CoolQCode.At(targetqq) + "释放了禁言术！ 但是什么都没有发生！");
-                    }
-
-                }
-            }
-
+            DealWithGroupMessageThread(subType, sendTime, fromGroup, fromQq, fromAnonymous, msg, font);
             return base.ProcessGroupMessage(subType, sendTime, fromGroup, fromQq, fromAnonymous, msg, font);
         }
+
         /// <summary>
-        /// 处理私聊信息
+        /// 处理群成员增加
         /// </summary>
         /// <param name="subType"></param>
         /// <param name="sendTime"></param>
+        /// <param name="fromGroup"></param>
         /// <param name="fromQq"></param>
-        /// <param name="msg"></param>
-        /// <param name="font"></param>
+        /// <param name="target"></param>
         /// <returns></returns>
-        public override int ProcessPrivateMessage(int subType, int sendTime, long fromQq, string msg, int font)
+        public override int ProcessGroupMemberIncrease(int subType, int sendTime, long fromGroup, long fromQq, long target)
         {
-            if (msg.Trim() == "test")
-            {
-                CoolQApi.SendPrivateMsg(fromQq, "无敌乔美大机霸！无敌乔美大机霸！无敌乔美大机霸！");
-            }
-            return base.ProcessPrivateMessage(subType, sendTime, fromQq, msg, font);
+            UpdateQQListThread(fromGroup);
+            return base.ProcessGroupMemberIncrease(subType, sendTime, fromGroup, fromQq, target);
         }
 
-
-        private bool CheckHasAtQQ(string msg, out long qq)
+        public override int ProcessMenuClickA()
         {
-            qq = 0;
-            bool flag = false;
+            //todo:do something 
+            return base.ProcessMenuClickA();
+        }
 
-            if (msg.Contains(CoolQCode.At(-1)))
-            {
-                flag = true;
-                qq = -1;
-                return flag;
-            }
+        public override int Enabled()
+        {
+            return base.Enabled();
+        }
 
-            List<string> numberStrList = new List<string>();
-            StringBuilder tempstr = new StringBuilder();
-            foreach (char c in msg)
+        #endregion
+
+        /// <summary>
+        /// 开启处理群消息进程
+        /// </summary>
+        /// <param name="subType"></param>
+        /// <param name="sendTime"></param>
+        /// <param name="fromGroup"></param>
+        /// <param name="fromQq"></param>
+        /// <param name="fromAnonymous"></param>
+        /// <param name="msg"></param>
+        /// <param name="font"></param>
+        private void DealWithGroupMessageThread(int subType, int sendTime, long fromGroup, long fromQq, string fromAnonymous, string msg, int font)
+        {
+            object[] paramaters = new object[] { subType, sendTime, fromGroup, fromQq, fromAnonymous, msg, font };
+            Thread mainThread = new Thread(new ParameterizedThreadStart(DealWithGroupMessage));
+            mainThread.Start(paramaters);
+        }
+        /// <summary>
+        /// 处理群消息方法
+        /// </summary>
+        /// <param name="obj"></param>
+        private void DealWithGroupMessage(object obj)
+        {
+            try
             {
-                if (Convert.ToInt32(c) >= 48 && Convert.ToInt32(c) <= 57)
+                object[] paramaters = (object[])obj;
+
+                long fromGroup = (long)paramaters[2];
+                long fromQq = (long)paramaters[3];
+                string msg = (string)paramaters[5];
+
+                if (!m_QQdics.ContainsKey(fromGroup))
                 {
-                    tempstr.Append(c);
+                    //获取群成员 需要api权限160
+                    List<GroupMemberInfo> memebers = CoolQApi.GetGroupMemberList(fromGroup).Model.ToList();
+                    m_QQdics.Add(fromGroup, memebers.Select(t => t.Number).ToList());
                 }
-                else
+
+                List<long> qqList = CheckHasAtQQ(m_QQdics[fromGroup], msg);
+
+                if (qqList.Count > 0)
                 {
-                    string temp = tempstr.ToString();
-                    if (!string.IsNullOrEmpty(temp))
+                    //禁言部分
+                    if (msg.Contains("禁言") || msg.Contains("闭嘴") || msg.Contains("别说话") || msg.Contains("让你说话了么") || msg.Contains("ShutUp"))
                     {
-                        numberStrList.Add(temp);
+                        foreach (var targetqq in qqList)
+                        {
+                            //全体成员跳过
+                            if (targetqq == -1)
+                            {
+                                continue;
+                            }
+
+                            //对该机器人
+                            if (targetqq == CoolQApi.GetLoginQQ())
+                            {
+                                CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + " 小逼，想禁言我？找死是吧！3分钟不谢！");
+                                CoolQApi.SetGroupBan(fromGroup, fromQq, 180);
+                                break;
+                            }
+
+                            //成功概率1/3
+                            bool success = m_Rand.Next(4200) % 3 == 1;
+                            if (success)
+                            {
+                                CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + "对" + CoolQCode.At(targetqq) + "释放了禁言术！ 成功了！  " + CoolQCode.At(targetqq) + "被禁言3分钟！");
+                                CoolQApi.SetGroupBan(fromGroup, targetqq, 180);
+                            }
+                            else
+                            {
+                                CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + "对" + CoolQCode.At(targetqq) + "释放了禁言术！ 但是什么都没有发生！");
+                            }
+                        }
+                    }
+                    //踢出群部分
+                    if (msg.Contains("滚") || msg.Contains("KickOut") || msg.Contains("踢了") || msg.Contains("飞踢") || msg.Contains("ThaiKick"))
+                    {
+                        foreach (var targetqq in qqList)
+                        {
+                            //全体成员跳过
+                            if (targetqq == -1)
+                            {
+                                continue;
+                            }
+                            //对该机器人
+                            if (targetqq == CoolQApi.GetLoginQQ())
+                            {
+                                CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + " 小逼，想踢我？找死是吧！翻滚吧！");
+                                CoolQApi.SetGroupKick(fromGroup, fromQq, false);
+                                break;
+                            }
+                            //成功概率1/5
+                            bool success = m_Rand.Next(4200) % 5 == 1;
+                            if (success)
+                            {
+                                CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + "狠狠踢了" + CoolQCode.At(targetqq) + "一脚！ " + CoolQCode.At(targetqq) + "飞了好远，直到飞出了群");
+                                CoolQApi.SetGroupKick(fromGroup, targetqq, false);
+                            }
+                            else
+                            {
+                                CoolQApi.SendGroupMsg(fromGroup, CoolQCode.At(fromQq) + "想要狠狠踢" + CoolQCode.At(targetqq) + "一脚，结果没有踢着，把脚崴了。好惨！");
+                            }
+                        }
                     }
                 }
             }
-            if (numberStrList.Count == 0)
+            catch (Exception e)
             {
-                return flag;
+                CoolQApi.AddLog(1, CoolQLogLevel.Error, e.Message);
             }
-            List<long> numbers = new List<long>();
-            numberStrList.ForEach(t => numbers.Add(long.Parse(t)));
 
-            foreach (long q in numbers)
+        }
+
+        /// <summary>
+        /// 获取信息中@的qq列表
+        /// </summary>
+        /// <param name="allqqList">群里所有qq成员</param>
+        /// <param name="msg">信息</param>
+        /// <returns></returns>
+        private List<long> CheckHasAtQQ(List<long> allqqList, string msg)
+        {
+            List<long> qqList = new List<long>();
+            bool flag = false;
+
+            Dictionary<long, string> QQAtDic = new Dictionary<long, string>();
+
+            QQAtDic.Add(-1, CoolQCode.At(-1));
+
+            foreach (var tempqq in allqqList)
             {
-                if (msg.Contains(CoolQCode.At(q)))
+                QQAtDic.Add(tempqq, CoolQCode.At(tempqq));
+            }
+
+            foreach (var qqkey in QQAtDic.Keys)
+            {
+                if (msg.Contains(QQAtDic[qqkey]))
                 {
-                    flag = true;
-                    qq = q;
-                    return flag;
+                    qqList.Add(qqkey);
                 }
             }
+            return qqList;
+        }
 
-            return flag;
+        /// <summary>
+        /// 更新QQ列表进程
+        /// </summary>
+        /// <param name="groupqq"></param>
+        private void UpdateQQListThread(long groupqq)
+        {
+            object[] paramaters = new object[] { groupqq };
+            Thread mainThread = new Thread(new ParameterizedThreadStart(UpdateQQList));
+            mainThread.Start(paramaters);
+        }
+
+        /// <summary>
+        /// 更新群里qq列表
+        /// </summary>
+        /// <param name="groupQQ"></param>
+        private void UpdateQQList(object obj)
+        {
+            object[] paramaters = (object[])obj;
+
+            long groupQQ = (long)paramaters[0];
+
+            //获取群成员 需要api权限160
+            List<GroupMemberInfo> memebers = CoolQApi.GetGroupMemberList(groupQQ).Model.ToList();
+
+            if (!m_QQdics.ContainsKey(groupQQ))
+            {
+                m_QQdics.Add(groupQQ, memebers.Select(t => t.Number).ToList());
+            }
+            else
+            {
+                m_QQdics[groupQQ] = memebers.Select(t => t.Number).ToList();
+            }
         }
     }
 }
